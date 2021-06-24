870409a2ad6df099036f76d4c6f50b21548bab9f
Dji <zagryadskiy.dima@gmail.com>
2021-06-24 21:26:12 +0500

    HomeWork_3.5 done

--- HomeWorkApp3_5.java
+++ HomeWorkApp3_5.java
@@ -1,12 +1,15 @@
 package ru.gb.level3.lesson5;
 
+import java.util.concurrent.BrokenBarrierException;
+import java.util.concurrent.CyclicBarrier;
+
 public class HomeWorkApp3_5 {
 
     public static final int CARS_COUNT = 4;
 
         public static void main(String[] args) {
             System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Подготовка!!!");
-            Race race = new Race(new Road(60), new Tunnel(), new Road(40));
+            Race race = new Race(CARS_COUNT, new Road(60), new Tunnel(CARS_COUNT), new Road(40));
             Car[] cars = new Car[CARS_COUNT];
             for (int i = 0; i < cars.length; i++) {
                 cars[i] = new Car(race, 20 + (int) (Math.random() * 10));
@@ -14,7 +17,17 @@ public class HomeWorkApp3_5 {
             for (int i = 0; i < cars.length; i++) {
                 new Thread(cars[i]).start();
             }
+            try {
+                race.getRaceCDL().await();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
             System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Гонка началась!!!");
+            try {
+                race.getPhaseCDL().await();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
             System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Гонка закончилась!!!");
         }
     }
